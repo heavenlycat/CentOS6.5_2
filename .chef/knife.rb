1840f8ca05ea413cd5a326d5b76cc94dbd0cfca9
heavenlycat <catbadsadmad@live.jp>
2014-11-06 16:15:57 +0900

    First commit

---
+++ .chef/knife.rb
@@ -0,0 +1,8 @@
+log_level                :info
+log_location             STDOUT
+node_name                'vagrant'
+client_key               'C:/HashiCorp/Vagrant/Project/CentOS6.5/.chef/vagrant.pem'
+validation_client_name   'chef-validator'
+validation_key           'C:/etc/chef-server/chef-validator.pem'
+chef_server_url          'https://AACWIN0502.aac-lan.aac.co.jp:443'
+syntax_check_cache_path  'C:/HashiCorp/Vagrant/Project/CentOS6.5/.chef/syntax_check_cache'
---
+++ Berksfile
@@ -0,0 +1,4 @@
+source "https://supermarket.getchef.com"
+cookbook "dstat", path: "./site-cookbooks/dstat"
+cookbook "apache", path: "./site-cookbooks/apache"
+cookbook "mysql", path: "./site-cookbooks/mysql"
